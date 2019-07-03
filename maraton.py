f74557f3c87d2075e7a58c55d81663d1085a3aaf
Nayiib <42815728+Nayiib@users.noreply.github.com>
2019-07-03 18:38:35 -0500

    Add files via upload

--- maraton.py
+++ maraton.py
@@ -1,6 +1,19 @@
-j=int(input("ingrese el diametro del lago"))
-k=int(input("ingrese el k de la caña"))
-k1=k/100
-c=((k1*k1)-((j*j)/4))/(-2*k1)
-  
-print ("la profundidad es ",round(c,1))
+i=1
+r=0
+lista=[]
+while i!=0:
+    datos=input()
+    listaDatos=datos.split(' ')
+    j=int(listaDatos[0])
+    k=int(listaDatos[1])
+    if j!=0 and k!=0:    
+        k1=k/100
+        c=((k1*k1)-((j*j)/4))/(-2*k1)
+        lista.insert(r,c)
+        r += 1
+    else: 
+        i=j+k 
+    if i==0:
+        for x in range (0, r):
+            print (round(lista[x],1) ) 
+        break
